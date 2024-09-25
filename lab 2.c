443b5d55d73457d4af4d2f3b3d46888606f64e13
afrahrasheed <afrarasheed003@gmail.com>
2024-09-25 16:13:55 +0400

    Add files via upload

---
+++ lab 2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+//Espresso ingredients//
+#define e_cb 8;
+#define e_w 30;
+#define e_m 0;
+#define e_cho 0;
+
+//Cappucino ingredients//
+#define c_cb 8;
+#define c_w 30;
+#define c_m 70;
+#define c_cho 0;
+
+//Mocha ingredients//
+#define m_cb 8;
+#define m_w 30;
+#define m_m 70;
+#define m_cho 30;
+
+int main() 
+{
+    
+printf("")
+//admin password required for coffee mmaker operator//
+#define admin_password "admin123"
+
+
+#define total_amount ""
