0680bb5517cb5f8a13ce83bdb1ba07d8cc73beed
afrahrasheed <afrarasheed003@gmail.com>
2024-09-25 16:26:14 +0400

    Add files via upload

--- lab 2.c
+++ lab 2.c
@@ -3,22 +3,22 @@
 #include <string.h>
 
 //Espresso ingredients//
-#define e_cb 8;
-#define e_w 30;
-#define e_m 0;
-#define e_cho 0;
+#define e_cb 8;      //grams of coffee beans added//
+#define e_w 30;      //water millimeter added//
+#define e_m 0;       //milk added in millimeter//
+#define e_cho 0;     //chocolate syrup added//
 
 //Cappucino ingredients//
-#define c_cb 8;
-#define c_w 30;
-#define c_m 70;
-#define c_cho 0;
+#define c_cb 8;     //grams of coffee beans added//
+#define c_w 30;     //water millimeter added//
+#define c_m 70;     //milk added in millimeter//
+#define c_cho 0;     //chocolate syrup added//
 
 //Mocha ingredients//
-#define m_cb 8;
-#define m_w 30;
-#define m_m 70;
-#define m_cho 30;
+#define m_cb 8;    //grams of coffee beans added//
+#define m_w 30;     //water millimeter added//
+#define m_m 70;     //milk added in millimeter//
+#define m_cho 30;   //chocolate syrup added//
 
 int main() 
 {
